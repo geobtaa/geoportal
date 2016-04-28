c79c360261db9ffa91242b89ba74387de27c3a0b
swadm <swadm@lib-geoblacklightdev.oit.umn.edu>
2016-04-28 13:21:19 -0500

    Initial cap dev setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.4.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'geoblacklight'
+set :repo_url, 'git@github.umn.edu:Libraries/geoblacklight.git'
 
 set :passenger_restart_with_touch, true
 
@@ -10,7 +10,7 @@ set :passenger_restart_with_touch, true
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/swadm/usr/local/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,7 +23,7 @@ set :passenger_restart_with_touch, true
 # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
@@ -35,7 +35,7 @@ set :passenger_restart_with_touch, true
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
