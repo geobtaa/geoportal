91f755b2d2546ee7c8fea45fdaf15e43fdfb4dd0
Michael Berkowski <mjb@umn.edu>
2016-05-02 12:31:34 -0500

    Updated paths for solr start script with env vars

--- rbenv-start-solr.sh
+++ rbenv-start-solr.sh
@@ -5,4 +5,5 @@
 #
 export PATH=/swadm/usr/local/rbenv/bin:/swadm/usr/local/rbenv/shims:$PATH;
 eval "$(rbenv init -)";
-RBENV_VERSION=2.3.0 BUNDLE_GEMFILE=/swadm/usr/local/geoblacklightdev/Gemfile bundle exec rake solr:start
+cd /swadm/usr/local/geoblacklight/current
+RBENV_VERSION=2.3.0 BUNDLE_GEMFILE=/swadm/usr/local/geoblacklight/current/Gemfile bundle exec rake solr:start
