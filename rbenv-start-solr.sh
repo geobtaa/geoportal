d9578eb396f42148e8a976cf59f1e132245cc36c
Michael Berkowski <mjb@umn.edu>
2016-05-09 14:53:27 -0500

    Eliminate solr_wrapper & rbenv from production solr start procedure

--- rbenv-start-solr.sh
+++ rbenv-start-solr.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #
-# Start jetty wrapper around Solr, first bootstrapping rbenv
-# Run from cron as /swadm/usr/local/geoblacklightdev/rbenv-start-jetty.sh
-#
-export PATH=/swadm/usr/local/rbenv/bin:/swadm/usr/local/rbenv/shims:$PATH;
-eval "$(rbenv init -)";
-cd /swadm/usr/local/geoblacklight/current
-RBENV_VERSION=2.3.0 BUNDLE_GEMFILE=/swadm/usr/local/geoblacklight/current/Gemfile bundle exec rake solr:start
+# Start solr with a simple bin/solr start
+cd /swadm/usr/local/solr && bin/solr start
