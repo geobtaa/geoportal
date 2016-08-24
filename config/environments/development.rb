cf2beb4053f73c676a2ba4930b9795ac562d490a
Eric Larson <ewlarson@umn.edu>
2016-08-24 10:24:02 -0500

    Mailer: config for dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.default_options = {from: 'no-reply@geo.btaa.org'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
