708c23f0d6f75f9783e4df531e662a725e9a47e0
Eric Larson <ewlarson@umn.edu>
2016-08-24 09:15:06 -0500

    Dev: test default sendmail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
