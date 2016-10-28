b06e959b2acde5757c422f74ac8902b82abd3fbf
Michael Berkowski <mjb@umn.edu>
2016-10-28 10:42:23 -0500

    Nightly db cleanup tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,15 @@
 set :job_template, "bash -l -c 'export PATH=:rbenv_path/bin::rbenv_path/shims:$PATH; eval \"$(rbenv init -)\"; RAILS_ENV=:environment RBENV_VERSION=:rbenv_ruby BUNDLE_GEMFILE=:path/Gemfile :job'"
 
 job_type :sitemap_refresh, 'cd :path && bundle exec rake sitemap:refresh'
+job_type :user_cleanup, 'cd :path && bundle exec rake devise_guests:delete_old_guest_uesrs[2]'
+job_type :search_cleanup, 'cd :path && bundle exec rake geoblacklight:delete_old_searches[7]'
 
 every :day, at: '12:30am', roles: [:app] do
   sitemap_refresh nil
+end
+every :day, at: '1:30am', roles: [:app] do
+  user_cleanup nil
+end
+every :day, at: '2:30am', roles: [:app] do
+  search_cleanup nil
 end
