3b5f6f2c2686088562480d845a9ac9ab23db4cf8
Michael Berkowski <mjb@umn.edu>
2016-10-30 15:43:47 -0500

    Correct job name for search cleanup

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :job_template, "bash -l -c 'export PATH=:rbenv_path/bin::rbenv_path/shims:$P
 
 job_type :sitemap_refresh, 'cd :path && bundle exec rake sitemap:refresh'
 job_type :user_cleanup, 'cd :path && bundle exec rake devise_guests:delete_old_guest_users[2]'
-job_type :search_cleanup, 'cd :path && bundle exec rake geoblacklight:delete_old_searches[7]'
+job_type :search_cleanup, 'cd :path && bundle exec rake blacklight:delete_old_searches[7]'
 
 every :day, at: '12:30am', roles: [:app] do
   sitemap_refresh nil
