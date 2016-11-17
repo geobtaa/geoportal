cbd38c716b0b70485ebd97b2a2ce5cdbc0b7561e
Eric Larson <ewlarson@umn.edu>
2016-11-17 14:07:43 -0600

    Tests: ErrorsController working

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
+  test "bad facet id should return not_found" do
+    assert_raises(ActionController::RoutingError) do
+      get '/catalog/facet/about'
+    end
+  end
+
   test "should get not_found" do
-    get errors_not_found_url
+    get '/404'
     assert_response :success
   end
 
   test "should get internal_server_error" do
-    get errors_internal_server_error_url
+    get '/500'
     assert_response :success
   end
-
 end
