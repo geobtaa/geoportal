a080b10eb67ee0bbbcedde1ff6ed64ad1b93f3a4
Eric Larson <ewlarson@umn.edu>
2016-12-20 15:45:27 -0600

    Tests: add poltergeist for remote testing canonical items

---
+++ test/integration/show_page_test.rb
@@ -0,0 +1,136 @@
+require "test_helper"
+
+class ShowPageTest < Capybara::Rails::TestCase
+
+  def setup
+    # Remote testing the show page features
+    Capybara.app_host = 'https://geo.btaa.org'
+  end
+
+  def test_purdue_shapefile_show
+    visit "/catalog/Purdue-urn-24bde658-53ff-41a7-9c62-15c5204a497c"
+
+    # Type
+    click_link 'Web services'
+
+    # WMS Web Service
+    assert page.has_content?("Web Mapping Service (WMS)")
+    assert page.has_selector?("#wms_webservice")
+
+    # Esri Web Service
+    assert page.has_content?("ArcGIS Dynamic Map Layer")
+    assert page.has_selector?("#dynamic_map_layer_webservice")
+
+    # ISO Metadata
+    assert page.has_link?("Metadata")
+
+    # IIIF Image
+    assert page.has_no_content?("iiif")
+
+    # Download
+    assert page.has_content?("Download Shapefile")
+
+    # Provenance
+    assert page.has_content?("Purdue")
+  end
+
+  def test_wisconsin_geodatabase_show
+    visit "/catalog/Wisconsin-urn-656ed665-63fc-45d9-96ab-465a07488f91"
+
+    # Type
+    assert page.has_no_link?("Web services")
+
+    # ISO Metadata
+    assert page.has_link?("Metadata")
+
+    # IIIF Image
+    assert page.has_no_content?("iiif")
+
+    # Download
+    assert page.has_content?("Download Geodatabase")
+
+    # Provenance
+    assert page.has_content?("Wisconsin")
+  end
+
+  def test_minnesota_shapefile_show
+    visit "/catalog/Minnesota-urn-480d1dc0-5fdd-42e2-b21c-7488d2496f7f"
+
+    # Type
+    click_link 'Web services'
+
+    # WMS Web Service
+    assert page.has_no_content?("Web Mapping Service (WMS)")
+    assert page.has_no_selector?("#wms_webservice")
+
+    # Esri Web Service
+    assert page.has_content?("ArcGIS Dynamic Map Layer")
+    assert page.has_selector?("#dynamic_map_layer_webservice")
+
+    # ISO Metadata
+    assert page.has_link?("Metadata")
+
+    # IIIF Image
+    assert page.has_no_content?("iiif")
+
+    # Download
+    assert page.has_content?("Download Shapefile")
+
+    # Provenance
+    assert page.has_content?("Minnesota")
+  end
+
+  def test_minnesota_tiff_show
+    visit "/catalog/minnesota-129514e1-a6d2-404e-8d44-077adee866e2"
+
+    # Type
+    assert page.has_no_link?("Web services")
+
+    # WMS Web Service
+    assert page.has_no_content?("Web Mapping Service (WMS)")
+    assert page.has_no_selector?("#wms_webservice")
+
+    # Esri Web Service
+    assert page.has_no_content?("ArcGIS Dynamic Map Layer")
+    assert page.has_no_selector?("#dynamic_map_layer_webservice")
+
+    # ISO Metadata
+    assert page.has_no_link?("Metadata")
+
+    # IIIF Image
+    assert page.has_no_content?("iiif")
+
+    # Download
+    assert page.has_content?("Download Tiff")
+
+    # Provenance
+    assert page.has_content?("Minnesota")
+  end
+
+  def test_iowa_map_show
+    visit "/catalog/iowa-urn-f1b57f24-d474-4493-90e1-d3c25f39b65b"
+
+    # Type
+    click_link 'Web services'
+
+    # WMS Web Service
+    assert page.has_no_content?("Web Mapping Service (WMS)")
+    assert page.has_no_selector?("#wms_webservice")
+
+    # Esri Web Service
+    assert page.has_content?("ArcGIS Dynamic Map Layer")
+    assert page.has_selector?("#dynamic_map_layer_webservice")
+
+    # ISO Metadata
+    assert page.has_no_link?("Metadata")
+
+    # IIIF Image
+    assert page.has_no_content?("iiif")
+
+    # Download
+    assert page.has_no_content?("Download Tiff")
+
+    # Provenance
+    assert page.has_content?("Iowa")
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/rails/capybara"
+require 'capybara/poltergeist'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 end
+
+# Ignore JS errors
+Capybara.register_driver :poltergeist_ignore_js_errors do |app|
+  Capybara::Poltergeist::Driver.new(app, {phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=TLSv1'], js_errors: false})
+end
+
+Capybara.default_driver = :poltergeist_ignore_js_errors
