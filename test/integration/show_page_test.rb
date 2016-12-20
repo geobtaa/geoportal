0b57cc33bcf23ce99e23cea17262363116988e42
Eric Larson <ewlarson@umn.edu>
2016-12-20 15:55:44 -0600

    Tests: catalog show exemplars

--- test/integration/show_page_test.rb
+++ test/integration/show_page_test.rb
@@ -9,6 +9,7 @@ class ShowPageTest < Capybara::Rails::TestCase
 
   def test_purdue_shapefile_show
     visit "/catalog/Purdue-urn-24bde658-53ff-41a7-9c62-15c5204a497c"
+    assert page.has_content?("Bedrock Geology: Indiana, 2010")
 
     # Type
     click_link 'Web services'
@@ -36,6 +37,7 @@ class ShowPageTest < Capybara::Rails::TestCase
 
   def test_wisconsin_geodatabase_show
     visit "/catalog/Wisconsin-urn-656ed665-63fc-45d9-96ab-465a07488f91"
+    assert page.has_content?("Hydrography (1:24,000), Wisconsin 2015")
 
     # Type
     assert page.has_no_link?("Web services")
@@ -55,6 +57,7 @@ class ShowPageTest < Capybara::Rails::TestCase
 
   def test_minnesota_shapefile_show
     visit "/catalog/Minnesota-urn-480d1dc0-5fdd-42e2-b21c-7488d2496f7f"
+    assert page.has_content?("Polling Places: Carver County, Minnesota, 2014")
 
     # Type
     click_link 'Web services'
@@ -82,6 +85,7 @@ class ShowPageTest < Capybara::Rails::TestCase
 
   def test_minnesota_tiff_show
     visit "/catalog/minnesota-129514e1-a6d2-404e-8d44-077adee866e2"
+    assert page.has_content?("Burritt's sectional and township map of Minnesota")
 
     # Type
     assert page.has_no_link?("Web services")
@@ -98,7 +102,7 @@ class ShowPageTest < Capybara::Rails::TestCase
     assert page.has_no_link?("Metadata")
 
     # IIIF Image
-    assert page.has_no_content?("iiif")
+    assert page.has_selector?("div[data-protocol='Iiif']")
 
     # Download
     assert page.has_content?("Download Tiff")
@@ -109,6 +113,7 @@ class ShowPageTest < Capybara::Rails::TestCase
 
   def test_iowa_map_show
     visit "/catalog/iowa-urn-f1b57f24-d474-4493-90e1-d3c25f39b65b"
+    assert page.has_content?("An Illustrated Historical Atlas of Des Moines County, Iowa, 1873")
 
     # Type
     click_link 'Web services'
