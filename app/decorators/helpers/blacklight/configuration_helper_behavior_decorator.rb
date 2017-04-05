ff1330783613128f6096d269e98e17a002c12f36
Eric Larson <ewlarson@umn.edu>
2017-04-05 13:12:47 -0500

    UI: pluralize label -- move to decorator pattern

---
+++ app/decorators/helpers/blacklight/configuration_helper_behavior_decorator.rb
@@ -0,0 +1,15 @@
+Blacklight::ConfigurationHelperBehavior.class_eval do
+  ##
+  # @CUSTOMIZED: added label pluralize call
+  # Look up the label for the show field
+  def document_show_field_label document, field
+    field_config = document_show_fields(document)[field]
+
+    field_label(
+      :"blacklight.search.fields.show.#{field}",
+      :"blacklight.search.fields.#{field}",
+      (field_config.label if field_config),
+      field.to_s.humanize
+    ).pluralize(Array.wrap(document.fetch(field, field)).count)
+  end
+end
