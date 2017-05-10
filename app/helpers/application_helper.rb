2c21333df80053755222ddb7829c04b04e3ecfc7
Eric Larson <ewlarson@umn.edu>
2017-05-10 15:52:13 -0500

    Thumbnail/Zoomable work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ require 'json'
 module ApplicationHelper
 
   def thumbnail_tag(doc)
-    content_tag(:a, :href => doc.thumbnail_link, :title => "Visit original source") do
+    content_tag(:a, :href => doc.thumbnail_link, :title => "Visit original") do
       image_tag(doc.thumbnail_image, alt: doc._source["title"], class: 'img img-responsive')
     end
   end
