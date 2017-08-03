492ceb0e47cefe327a884e23ef0e6a06ab7e27cd
Eric Larson <ewlarson@umn.edu>
2017-08-03 13:26:30 -0500

    Home.js updates

--- app/assets/javascripts/geoblacklight/modules/home.js
+++ app/assets/javascripts/geoblacklight/modules/home.js
@@ -15,20 +15,13 @@ Blacklight.onLoad(function() {
 
     var pruneCluster = new PruneClusterForLeaflet();
 
-    // Oboe - steam results
-    // Select example: http://localhost:8983/solr/geoportal/select?fl=uuid_sdv,dc_title_sdv,centroid_sdv&indent=on&q=*:*&wt=json
-    // Export example: http://localhost:8983/solr/geoportal/export?fl=uuid_sdv,dc_title_sdv,centroid_sdv&indent=on&q=*:*&wt=json&sort=dc_title_sdv%20asc
+    // Oboe - SAX steam JSON results from Solr /export
     oboe('http://localhost:8983/solr/geoportal/export?fl=uuid_sdv,dc_title_sdv,centroid_sdv&indent=on&q=*:*&wt=json&sort=dc_title_sdv%20asc&rows=10000')
       .node('response.docs.*', function( doc ){
 
           if(typeof doc.centroid_sdv != 'undefined'){
             var latlng = doc.centroid_sdv.split(",")
-
-            // console.log("Add: marker" + latlng[0] + "," + latlng[1])
-            // Create marker - adds title attribute
             var marker = new PruneCluster.Marker(latlng[0],latlng[1], {popup: "<a href='/catalog/" + doc.uuid_sdv + "'>" + doc.dc_title_sdv + "</a>"});
-            // Add popup and link to item
-            // marker.data.popup("<a href='/catalog/" + doc.uuid_sdv + "'>" + doc.dc_title_sdv + "</a>");
             pruneCluster.RegisterMarker(marker);
           }
         }
@@ -36,7 +29,5 @@ Blacklight.onLoad(function() {
       .done(function(){
         geoblacklight.map.addLayer(pruneCluster);
       })
-      // no need to handle update or exit set here since
-      // downloading is purely additive
   });
 });
