36cb65ac8e54035c56afe5c71d1d5ef994460800
Eric Larson <ewlarson@umn.edu>
2017-08-29 13:20:50 -0500

    JS: add metadata init call

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -5,6 +5,7 @@
 //= require geoblacklight/modules/collapse
 //= require geoblacklight/modules/download
 //= require geoblacklight/modules/metadata_download_button.js
+//= require geoblacklight/modules/metadata.js
 //= require geoblacklight/modules/geosearch
 //= require geoblacklight/modules/item
 //= require geoblacklight/modules/layer_opacity
