87cf5aef104520797d1a86a97f4ed294b6df1c52
Eric Larson <ewlarson@umn.edu>
2017-09-13 10:29:21 -0500

    App: require rake -- needed to access rails console
    The capistrano maintenance gem is tossing errors related to rake task
    namespace unless we require rake.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rake'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,7 +15,7 @@ module Geoblacklight
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
