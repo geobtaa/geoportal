805426e0bb962084b1ecbcf11286b3ade8fe8124
Eric Larson <ewlarson@umn.edu>
2017-12-19 15:35:04 -0600

    Deploy: config production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'lib-geoblacklight.oit.umn.edu', user: fetch(:deploy_user), roles: %w{app db web}
+server 'lib-geobl-prd.oit.umn.edu', user: fetch(:deploy_user), roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
