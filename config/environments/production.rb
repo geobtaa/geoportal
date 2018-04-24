edf1634d27beb02c0ba3505dd50ece2f7570692f
Eric Larson <ewlarson@umn.edu>
2018-04-24 09:09:58 -0500

    Bug fix: Set host for default_url_options
    Fixes #96

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@geo.btaa.org'}
+  config.action_mailer.default_url_options = { :host => 'geo.btaa.org' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
