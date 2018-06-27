65452e7b37d257a8a75063f5016c278037940192
Eric Larson <ewlarson@umn.edu>
2018-06-27 15:48:31 -0500

    Dev: don't delivery emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@geo.btaa.org'}
 
