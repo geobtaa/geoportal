37a5043b50d04aa24151b83ff5ca9733793f1e0a
Eric Larson <ewlarson@umn.edu>
2018-06-27 14:43:30 -0500

    Add: feedback form, nav button, mailer, gem, styles, etc.

---
+++ config/initializers/pointless_feedback.rb
@@ -0,0 +1,15 @@
+PointlessFeedback.setup do |config|
+  # ==> Feedback Configuration
+  # Configure the topics for the user to choose from on the feedback form
+  config.message_topics = ['Correction', 'Question', 'Comments or Suggestions', 'Other']
+
+  # ==> Email Configuration
+  # Configure feedback email properties (disabled by default)
+  # Variables needed for emailing feedback
+  config.email_feedback            = true
+  config.send_from_submitter       = true
+  # config.from_email                = 'no-reply@geo.btaa.org'
+  config.to_emails                 = ['ewlarson@umn.edu','btaa-gdp@umn.edu','geoportal@btaa.org']
+  config.google_captcha_site_key   = ENV["GEOBLACKLIGHT_CAPTCHA_SITE_KEY"]
+  config.google_captcha_secret_key = ENV["GEOBLACKLIGHT_CAPTCHA_SECRET_KEY"]
+end
