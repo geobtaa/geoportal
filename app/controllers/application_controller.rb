774c407dafb8bc835b762e4f7d15de9f6bff225b
Eric Larson <ewlarson@umn.edu>
2018-07-10 15:31:22 -0500

    App: remove save_previous_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,9 @@ class ApplicationController < ActionController::Base
   include Blacklight::Controller
   layout 'blacklight'
 
-  after_action :save_previous_url
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def save_previous_url
-    # session[:previous_url] is a Rails built-in variable to save last url.
-    session[:previous_url] = URI(request.referer || '').path
-  end
 
   protected
 
