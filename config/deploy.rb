e07619f0ef24ee4bf8890c3e15ddbfe11aaccd0d
Michael Berkowski <mjb@umn.edu>
2019-02-26 11:28:35 -0600

    merge conflict res in ruby-version

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/blacklight.yml', 'config/database.yml', 'config/solr.yml', 'config/secrets.yml')
-set :linked_files, []
+set :linked_files, ['config/database.yml']
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'public/system', 'public/uploads')
