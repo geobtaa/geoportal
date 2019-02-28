4d2d0c49596f7177f02c907ba7e4d5d5f77cb8bd
Michael Berkowski <mjb@umn.edu>
2019-02-28 15:48:16 -0600

    Thorough commenting of cron tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,24 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
-#set :job_template, "bash -l -c 'BUNDLE_GEMFILE=:path/Gemfile :job RAILS_ENV=:environment'"
-
-#job_type :sitemap_refresh, 'cd :path && bundle exec rake sitemap:refresh'
-#job_type :user_cleanup, 'cd :path && bundle exec rake devise_guests:delete_old_guest_users[2]'
-#job_type :search_cleanup, 'cd :path && bundle exec rake blacklight:delete_old_searches[7]'
 job_type :logrotate, '/usr/sbin/logrotate -s :path/../../shared/tmp/logrotate.state :path/config/logrotate.conf > /dev/null'
 
+# Harvest thumbnail images for search results
 every :day, at: '12:05am', roles: [:app] do
-  # Harvest thumbnail images for search results
   rake 'geoportal:queue_incomplete_states'
 end
 every :day, at: '12:30am', roles: [:app] do
   rake 'sitemap:refresh'
 end
+# Cleans up anonymous user accounts created by search sessions
 every :day, at: '1:30am', roles: [:app] do
   rake 'blacklight:delete_old_guest_users[2]'
 end
+# Cleans up recent anonymous search records
 every :day, at: '2:30am', roles: [:app] do
   rake 'blacklight:delete_old_searches[7]'
 end
+# Check image harvest state and email results
 every :day, at: '3:00am', roles: [:app] do
-  # Check image harvest state and email results
   rake 'geoportal:sidecar_states'
 end
 # URI analysis
@@ -38,6 +35,7 @@ every '0 8 2 * *', roles: [:app] do
   rake 'geoportal:uri_report'
 end
 
+# Log rotation does not use the template bash & env var wrapper
 set :job_template, nil
 every :day, at: '12:00am', roles: [:app] do
   logrotate nil
