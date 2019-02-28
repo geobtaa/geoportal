8fdb39c3a91a3bdc137a84b78075d59aff1b68a3
Michael Berkowski <mjb@umn.edu>
2019-02-28 15:39:02 -0600

    use built-in rake runner for cron tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,49 +2,40 @@
 # Learn more: http://github.com/javan/whenever
 set :job_template, "bash -l -c 'BUNDLE_GEMFILE=:path/Gemfile :job RAILS_ENV=:environment'"
 
-job_type :sitemap_refresh, 'cd :path && bundle exec rake sitemap:refresh'
-job_type :user_cleanup, 'cd :path && bundle exec rake devise_guests:delete_old_guest_users[2]'
-job_type :search_cleanup, 'cd :path && bundle exec rake blacklight:delete_old_searches[7]'
+#job_type :sitemap_refresh, 'cd :path && bundle exec rake sitemap:refresh'
+#job_type :user_cleanup, 'cd :path && bundle exec rake devise_guests:delete_old_guest_users[2]'
+#job_type :search_cleanup, 'cd :path && bundle exec rake blacklight:delete_old_searches[7]'
 job_type :logrotate, '/usr/sbin/logrotate -s :path/../../shared/tmp/logrotate.state :path/config/logrotate.conf > /dev/null'
 
-# Harvest thumbnail images for search results
-job_type :harvest_images, 'cd :path && bundle exec rake geoportal:queue_incomplete_states'
-
-# Check image harvest state and email results
-job_type :email_image_harvest_results, 'cd :path && bundle exec rake geoportal:sidecar_states'
-
-# URI Analysis tasks
-job_type :uri_purge, 'cd :path && bundle exec rake geoportal:uri_purge'
-job_type :uri_process_all, 'cd :path && bundle exec rake geoportal:uri_process_all'
-job_type :uri_process_incomplete_states, 'cd :path && bundle exec rake geoportal:uri_queue_incomplete_states'
-job_type :uri_report, 'cd :path && bundle exec rake geoportal:uri_report'
-
 every :day, at: '12:05am', roles: [:app] do
-  harvest_images nil
+  # Harvest thumbnail images for search results
+  rake 'geoportal:queue_incomplete_states'
 end
 every :day, at: '12:30am', roles: [:app] do
-  sitemap_refresh nil
+  rake 'sitemap:refresh'
 end
 every :day, at: '1:30am', roles: [:app] do
-  user_cleanup nil
+  rake 'blacklight:delete_old_guest_users[2]'
 end
 every :day, at: '2:30am', roles: [:app] do
-  search_cleanup nil
+  rake 'blacklight:delete_old_searches[7]'
 end
 every :day, at: '3:00am', roles: [:app] do
-  email_image_harvest_results nil
+  # Check image harvest state and email results
+  rake 'geoportal:sidecar_states'
 end
+# URI analysis
 every '0 1 1 * *', roles: [:app] do
-  uri_purge nil
+  rake 'geoportal:uri_purge'
 end
 every '0 2 1 * *', roles: [:app] do
-  uri_process_all nil
+  rake 'geoportal:uri_process_all'
 end
 every '0 1 2 * *', roles: [:app] do
-  uri_process_incomplete_states nil
+  rake 'geoportal:uri_queue_incomplete_states'
 end
 every '0 8 2 * *', roles: [:app] do
-  uri_report nil
+  rake 'geoportal:uri_report'
 end
 
 set :job_template, nil
