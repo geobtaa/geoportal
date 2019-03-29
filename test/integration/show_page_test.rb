bbd0bd18b86a4f33b894191a2827ea447aa6a4c4
Eric Larson <ewlarson@gmail.com>
2019-03-29 14:57:21 -0500

    Tests: no fullscreen toggle on static images for now.

--- test/integration/show_page_test.rb
+++ test/integration/show_page_test.rb
@@ -127,6 +127,7 @@ class ShowPageTest < Capybara::Rails::TestCase
   end
 
   def test_fullscreen_map_toggle
+    skip('No toggle on static images for now.')
     visit "/catalog/87adb12a-30b5-4bc3-866c-97adcd7e3d2e"
     assert page.has_selector?(".leaflet-control-fullscreen-button")
     click_on(class: 'leaflet-control-fullscreen-button')
