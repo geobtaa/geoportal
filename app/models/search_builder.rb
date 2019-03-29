ea007064ed4f4b239ff2d1d83af4d1ea60b88afc
Eric Larson <ewlarson@gmail.com>
2019-03-29 11:23:47 -0500

    SearchBuilder: remove pulmap-based override

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -5,18 +5,4 @@ class SearchBuilder < Blacklight::SearchBuilder
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
 
   include Geoblacklight::SpatialSearchBehavior
-
-  def add_spatial_params(solr_params)
-    if blacklight_params[:bbox]
-      solr_params[:bq] ||= []
-      solr_params[:bq] =
-        ["#{Settings.GEOMETRY_FIELD}:\"IsWithin(#{envelope_bounds})\"^300"]
-      solr_params[:fq] ||= []
-      solr_params[:fq] <<
-        "#{Settings.GEOMETRY_FIELD}:\"Intersects(#{envelope_bounds})\""
-    end
-    solr_params
-  rescue Geoblacklight::Exceptions::WrongBoundingBoxFormat
-    solr_params
-  end
 end
