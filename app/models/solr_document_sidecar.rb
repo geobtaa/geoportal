3c797847e2fe9da7801089ca15d63f5aabd7ab63
Eric Larson <ewlarson@gmail.com>
2019-08-26 16:37:56 -0500

    App: add active storage to SolrDocumentSidecar

--- app/models/solr_document_sidecar.rb
+++ app/models/solr_document_sidecar.rb
@@ -4,11 +4,16 @@
 # Metadata for indexed documents
 class SolrDocumentSidecar < ApplicationRecord
   include Statesman::Adapters::ActiveRecordQueries
+
+  # Deprecated - Carrierwave
   mount_uploader :image, ImageUploader
+
+  # ActiveStorage
+  has_one_attached :as_image
 
   belongs_to :document, required: true, polymorphic: true
   has_many :image_upload_transitions, autosave: false, dependent: :destroy
-  has_one_attached :image_new
+  has_many :sidecar_image_transitions, autosave: false, dependent: :destroy
 
   # If the sidecar is updated, need to re-fetch thumbnail image
   # after_update :reimage, if: :saved_change_to_version?
@@ -23,22 +28,36 @@ class SolrDocumentSidecar < ApplicationRecord
   end
 
   def state_machine
-    @state_machine ||= ImageUploadStateMachine.new(self, transition_class: ImageUploadTransition)
+    @state_machine ||= ImageUploadStateMachine.new(
+      self,
+      transition_class: ImageUploadTransition
+    )
+  end
+
+  def image_state
+    @image_state ||= SidecarImageStateMachine.new(
+      self,
+      transition_class: SidecarImageTransition
+    )
   end
 
   def self.transition_class
-    ImageUploadTransition
+    # ImageUploadTransition   - Old
+    SidecarImageTransition #  - New
   end
 
   def self.initial_state
     :initialized
   end
 
-  private_class_method :initial_state
+  def self.image_url
+    Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+  end
 
-  # private
-  #
-  # def reimage
-  #  StoreImageJob.perform_later(self.document.to_h)
-  # end
+  def reimage!
+    image.purge if image.attached?
+    GeoblacklightSidecarImages::StoreImageJob.perform_later(document.id)
+  end
+
+  private_class_method :initial_state
 end
