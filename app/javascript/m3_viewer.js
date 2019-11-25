05f6e44ed294c111891f9bc040ae27c3b0247f5c
Eric Larson <ewlarson@gmail.com>
2019-11-25 11:12:45 -0600

    Mirador: temp remove plugins

--- app/javascript/m3_viewer.js
+++ app/javascript/m3_viewer.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import Mirador from 'mirador/dist/es/src/index.js';
-import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js';
-import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
-import osdReferencePlugin from 'mirador-dl-plugin/es/OSDReferences.js';
+// import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js';
+// import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
+// import osdReferencePlugin from 'mirador-dl-plugin/es/OSDReferences.js';
 
 window.Mirador = Mirador;
-window.miradorDownloadPlugin = miradorDownloadPlugin;
-window.miradorDownloadDialogPlugin = miradorDownloadDialogPlugin;
-window.osdReferencePlugin = osdReferencePlugin;
+// window.miradorDownloadPlugin = miradorDownloadPlugin;
+// window.miradorDownloadDialogPlugin = miradorDownloadDialogPlugin;
+// window.osdReferencePlugin = osdReferencePlugin;
