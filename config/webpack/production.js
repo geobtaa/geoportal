d7e7df9f0f21c6290ff875fffefaa5c2e12f6ef7
Eric Larson <ewlarson@gmail.com>
2019-11-25 15:42:23 -0600

    Webpacker: update config to use *-loaders in prod only

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.loaders.prepend('sass', {
+    test: /\.(css|scss|sass)$/,
+    use: [{
+        loader: 'style-loader'
+    }, {
+        loader: 'css-loader'
+    }, {
+        loader: 'sass-loader',
+        options: {
+            includePaths: ['node_modules'],
+        }
+    }]
+})
+
 module.exports = environment.toWebpackConfig()
