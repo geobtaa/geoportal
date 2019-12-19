9e171aa61fe4f35a32c9393e9d76b14dcac845df
Eric Larson <ewlarson@gmail.com>
2019-12-19 12:54:56 -0600

    Mirador: remove from project (will revisit at official M3 release)

--- app/javascript/packs/m3.js
+++
@@ -1,8 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-import '@babel/polyfill';
-import 'url-polyfill/url-polyfill';
-import 'unfetch/polyfill';
-
-import M3Viewer from 'm3_viewer';
