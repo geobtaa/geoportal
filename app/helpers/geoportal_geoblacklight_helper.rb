5a8534c96fa84a57064713279c5d6e268c4e6278
Eric Larson <ewlarson@gmail.com>
2019-12-19 12:40:35 -0600

    IIIF Manifest: add UV viewer
    Adds UV as the iiif_manifest viewer, includes a fixture from PUL which includes IIIF presentation rendering blocks.

---
+++ app/helpers/geoportal_geoblacklight_helper.rb
@@ -0,0 +1,12 @@
+module GeoportalGeoblacklightHelper
+  include GeoblacklightHelper
+
+  def manifest_viewer
+    content_tag :div, nil, class: "uv" do
+      content_tag :iframe, nil,
+                  allowfullscreen: true,
+                  src: "#{asset_path("uv/uv")}#?manifest=#{@document.references.references(:iiif_manifest).endpoint}&config=#{asset_url('uv/uv_config.json')}"
+
+    end
+  end
+end
