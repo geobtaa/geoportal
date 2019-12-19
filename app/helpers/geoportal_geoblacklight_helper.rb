5fd4b12ee78f9b1f3b282dfc7dcf46f0dbcd335a
Eric Larson <ewlarson@gmail.com>
2019-12-19 12:54:03 -0600

    Helper: use skip_pipeline: true to direct to public dir asset

--- app/helpers/geoportal_geoblacklight_helper.rb
+++ app/helpers/geoportal_geoblacklight_helper.rb
@@ -5,7 +5,7 @@ module GeoportalGeoblacklightHelper
     content_tag :div, nil, class: "uv" do
       content_tag :iframe, nil,
                   allowfullscreen: true,
-                  src: "#{asset_path("uv/uv")}#?manifest=#{@document.references.references(:iiif_manifest).endpoint}&config=#{asset_url('uv/uv_config.json')}"
+                  src: "#{asset_path("uv/uv", skip_pipeline: true)}#?manifest=#{@document.references.references(:iiif_manifest).endpoint}&config=#{asset_url('uv/uv_config.json')}"
 
     end
   end
