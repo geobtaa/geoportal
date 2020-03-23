5ee53d19679f52493f1fdc9f2eab06c7cd4adddc
Eric Larson <ewlarson@gmail.com>
2020-03-23 11:52:02 -0500

    Citation: use dct_issued_s only; support n.d. for missing or 9999 entries
    Fixes #300

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,30 +23,26 @@ module ApplicationHelper
   def citation_dct_issued_s(dct_issued_s)
     date_string = ""
 
-    logger.debug("dct_issued_s: #{dct_issued_s}")
-
     if dct_issued_s.size == 4
       date_string = dct_issued_s
     elsif dct_issued_s.match?(/[-|:|\/]/)
       begin
-        date_string = Chronic.parse(dct_issued_s).to_date.strftime('%b %d, %Y')
+        date_string = Chronic.parse(dct_issued_s).to_date.strftime('%Y')
       rescue
-        date_string = dct_issued_s
+        begin
+          # Capture YYYY from '2004-01-00' / the '00' day value breaks Chronic
+          date_string = dct_issued_s.split("-")[0]
+        rescue
+          date_string = dct_issued_s
+        end
       end
     else
       date_string = dct_issued_s
     end
 
-    date_string
-  end
-
-  def citation_solr_year_i(solr_year_i)
-    date_string = ""
-
-    if solr_year_i.to_s.include?('9999')
-      date_string = "[unknown date]"
-    else
-      date_string = solr_year_i.to_s
+    # Guard against unknown dates
+    if date_string == '9999'
+      date_string = 'n.d.'
     end
 
     date_string
