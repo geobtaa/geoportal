80bd4d88ac196241ccd70c3fc5401711c1853c0d
Eric Larson <ewlarson@gmail.com>
2020-03-24 10:09:41 -0500

    Mirador: upgrade to latest releases, still with error
    "Function components cannot be given refs".

--- app/javascript/m3_viewer.js
+++ app/javascript/m3_viewer.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import Mirador from 'mirador/dist/es/src/index.js';
-import osdReferencePlugin from 'mirador-dl-plugin/es/OSDReferences.js';
+import OSDReferencesPlugin from 'mirador/dist/es/src/plugins/OSDReferences.js';
 import miradorDownloadPlugin from 'mirador-dl-plugin/es/miradorDownloadPlugin.js';
 import miradorDownloadDialogPlugin from 'mirador-dl-plugin/es/MiradorDownloadDialog.js';
 
@@ -9,4 +9,4 @@ window.Mirador = Mirador;
 window.miradorDownloadPlugin = miradorDownloadPlugin;
 
 window.miradorDownloadDialogPlugin = miradorDownloadDialogPlugin;
-window.osdReferencePlugin = osdReferencePlugin;
+window.OSDReferencesPlugin = OSDReferencesPlugin;
