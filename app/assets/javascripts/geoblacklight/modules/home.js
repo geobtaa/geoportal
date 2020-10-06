4cf2e3ec4b85a2e59d0f63f5d1c3698327b3c998
Eric Larson <ewlarson@gmail.com>
2020-10-06 13:31:26 -0500

    Feature/faster homepage (#338)
    * Homepage: faster cluster loading
    
    Add progress bar to map. Use chunkedLoading and addLayers.
    
    * Homepage: make centroids.json file smaller
    
    Short object keys, round lat/lng values to 4 decimals, truncate titles. And remove suppressed_b:true docs as well.
    
    * Centroids: call just the truncate method

--- app/assets/javascripts/geoblacklight/modules/home.js
+++ app/assets/javascripts/geoblacklight/modules/home.js
@@ -14,19 +14,39 @@ Blacklight.onLoad(function() {
       staticButton: '<a id="map-search-btn" class="btn btn-primary hidden-xs hidden-sm">Search here</a>'
     }));
 
-    var markers = L.markerClusterGroup();
+    var progress = document.getElementById('progress');
+    var progressBar = document.getElementById('progress-bar');
+
+    function updateProgressBar(processed, total, elapsed, layersArray) {
+      if (elapsed > 100) {
+        // if it takes more than a second to load, display the progress bar:
+        progress.style.display = 'block';
+        progressBar.style.width = Math.round(processed/total*100) + '%';
+      }
+
+      if (processed === total) {
+        // all markers processed - hide the progress bar:
+        progress.style.display = 'none';
+      }
+    }
+
+    var markers = L.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar });
+    var markerList = [];
 
     // Oboe - SAX steam JSON results from Solr /export
     // oboe('http://localhost:8983/solr/geoportal/export?fl=uuid_sdv,dc_title_sdv,centroid_sdv&indent=on&q=*:*&wt=json&sort=dc_title_sdv%20asc&rows=10000')
+
     oboe('/centroids.json')
       .node('*', function( doc ){
-          if(typeof doc.b1g_centroid_ss != 'undefined'){
-            var latlng = doc.b1g_centroid_ss.split(",")
-            markers.addLayer(L.marker([latlng[0],latlng[1]]).bindPopup("<a href='/catalog/" + doc.layer_slug_s + "'>" + doc.dc_title_s + "</a>"));
+          if(typeof doc.c != 'undefined'){
+            var latlng = doc.c.split(",")
+            var marker = L.marker([latlng[0],latlng[1]]).bindPopup("<a href='/catalog/" + doc.l + "'>" + doc.t + "</a>")
+            markerList.push(marker);
           }
         }
       )
       .done(function(){
+        markers.addLayers(markerList)
         geoblacklight.map.addLayer(markers);
       })
   });
--- lib/tasks/geoportal/generate_centroids_json.rake
+++ lib/tasks/geoportal/generate_centroids_json.rake
@@ -4,16 +4,19 @@ namespace :geoportal do
   desc 'Generate homepage centroids for map clustering'
   task generate_centroids_json: :environment do
     # Search request
-    response = Blacklight.default_index.connection.get 'select', :params => {:q => '*:*', :rows => '100000'}
+    response = Blacklight.default_index.connection.get 'select', :params => {:q => '!suppressed_b:true', :rows => '100000'}
 
     docs = []
     response["response"]["docs"].each_with_index do |doc, index|
       begin
         if doc.key?('b1g_centroid_ss') && !doc['b1g_centroid_ss'].empty?
           entry = {}
-          entry['layer_slug_s'] = doc['layer_slug_s']
-          entry['dc_title_s'] = doc['dc_title_s']
-          entry['b1g_centroid_ss'] = doc['b1g_centroid_ss']
+          entry['l'] = doc['layer_slug_s']
+          entry['t'] = ActionController::Base.helpers.truncate(doc['dc_title_s'], length: 50)
+          lat,lng    = doc['b1g_centroid_ss'].split(",")
+          lat = lat.to_f.round(4) # Truncate long values
+          lng = lng.to_f.round(4) # Truncate long values
+          entry['c'] = "#{lat},#{lng}"
           docs << entry
         end
       rescue Exception => e
