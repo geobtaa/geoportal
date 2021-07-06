bfa85b44d7f75e46fcb364fb680dd22981b57871
Eric Larson <ewlarson@gmail.com>
2021-07-06 12:48:32 -0500

    Sitemap: update for only published docs
    Fixes #378

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,8 @@
 solrinst = RSolr.connect url: Blacklight.connection_config[:url]
 
-# Select all the available docs from Solr
-response = solrinst.get('select', params: {q: '*:*', fl: 'layer_slug_s', rows: 999999})
+# Select all the "published" docs from Solr
+response = solrinst.get('select', params: {q: 'b1g_publication_state_s:published', fl: 'layer_slug_s', rows: 999999})
+
 # Build a flat sorted array of all document slugs
 slugs = response['response']['docs'].map { |doc| doc['layer_slug_s'] }.sort
 
