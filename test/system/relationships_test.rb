4a3b0a1ffb557567332607f16533060787d592ca
Eric Larson <ewlarson@gmail.com>
2021-12-09 17:02:22 -0600

    Bug fix: remove unpublished docs from relationships
    Fixes #412

--- test/system/relationships_test.rb
+++ test/system/relationships_test.rb
@@ -14,10 +14,19 @@ class RelationshipsTest < ApplicationSystemTestCase
 
     assert page.has_content?("Has part...")
     assert page.has_link?("Browse all 4 records...")
-
     click_link("Browse all 4 records...")
 
-    assert page.has_no_text?("No results found for your search")
+    assert page.has_no_content?("No results found for your search")
+  end
 
+  def test_child_relationships_hide_unpublished_docs
+    # Aitkin parent record
+    visit "/catalog/27001-road-centerlines"
+    assert page.has_content?("Aitkin County GIS")
+
+    # Relationships should not be present
+    assert page.has_no_selector?(".card.relations")
+    assert page.has_no_content?("Has version...")
+    assert page.has_no_link?("Road Centerlines [Aitkin County] 2021")
   end
 end
