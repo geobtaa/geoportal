279b0c07870c020b23401595eb9f380f5a4a15de
Eric Larson <ewlarson@gmail.com>
2023-09-08 15:03:42 -0500

    Update kithe.rb
    Set env var for Solr url

--- config/initializers/kithe.rb
+++ config/initializers/kithe.rb
@@ -4,7 +4,7 @@ require "kithe"
 
 traject_logger = ActiveSupport::Logger.new("#{Rails.root}/log/traject.log")
 
-Kithe.indexable_settings.solr_url = ENV["SOLR_URL"]
+Kithe.indexable_settings.solr_url = ENV["GEOBLACKLIGHT_SOLR_URL"]
 
 # Index to solr with solr `id` field being our friendlier_id
 Kithe.indexable_settings.solr_id_value_attribute = :friendlier_id
