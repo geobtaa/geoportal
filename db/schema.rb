6719638fa417397124c8b946d4757f91ba8ba333
Eric Larson <ewlarson@gmail.com>
2023-09-11 14:32:26 -0500

    DB: migration to add statesman related tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_05_205207) do
+ActiveRecord::Schema.define(version: 2023_09_11_192235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
