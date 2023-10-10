bfea9a564dde06974c562f55e108a9b272edb5f8
Eric Larson <ewlarson@gmail.com>
2023-10-10 09:59:39 -0500

    Update 20231009160910_add_gbl_admin.rb
    Remove invitation cols from migration.

--- db/migrate/20231009160910_add_gbl_admin.rb
+++ db/migrate/20231009160910_add_gbl_admin.rb
@@ -368,19 +368,19 @@ class AddGblAdmin < ActiveRecord::Migration[6.1]
     change_column :uri_transitions, :solr_document_uri_id, :bigint
 
     # users
-    add_column :users, :invitation_token, :string
-    add_column :users, :invitation_created_at, :datetime
-    add_column :users, :invitation_sent_at, :datetime
-    add_column :users, :invitation_accepted_at, :datetime
-    add_column :users, :invitation_limit, :integer
-    add_column :users, :invited_by_type, :string
-    add_column :users, :invited_by_id, :bigint
-    add_column :users, :invitations_count, :integer, default: 0
-    add_column :users, :admin, :boolean, default: false, null: false
-    add_index :users, :invitation_token, unique: true
-    add_index :users, :invitations_count
-    add_index :users, :invited_by_id
-    add_index :users, [:invited_by_type, :invited_by_id]
+    # add_column :users, :invitation_token, :string
+    # add_column :users, :invitation_created_at, :datetime
+    # add_column :users, :invitation_sent_at, :datetime
+    # add_column :users, :invitation_accepted_at, :datetime
+    # add_column :users, :invitation_limit, :integer
+    # add_column :users, :invited_by_type, :string
+    # add_column :users, :invited_by_id, :bigint
+    # add_column :users, :invitations_count, :integer, default: 0
+    # add_column :users, :admin, :boolean, default: false, null: false
+    # add_index :users, :invitation_token, unique: true
+    # add_index :users, :invitations_count
+    # add_index :users, :invited_by_id
+    # add_index :users, [:invited_by_type, :invited_by_id]
 
     create_table "versions", force: :cascade do |t|
       t.string "item_type", null: false
