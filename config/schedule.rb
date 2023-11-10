8c01f86c8bc9aa27e39fdbc297129338d96b5306
Eric Larson <ewlarson@gmail.com>
2023-11-10 13:48:13 -0600

    Schedule: remove geoportal:export_data job
    Fixes #546

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,10 +24,13 @@ end
 every :day, at: '2:30am', roles: [:app] do
   rake 'blacklight:delete_old_searches[7]'
 end
+
+# No longer required - EWL 11/10/23
 # Exports SOLR data to public/data.json
-every '30 3 1 * *', roles: [:app] do
-  rake 'geoportal:export_data'
-end
+# every '30 3 1 * *', roles: [:app] do
+#  rake 'geoportal:export_data'
+# end
+
 # Clean Carrierwave tmp file directory
 every :day, at: '4:30 am', roles: [:app] do
   runner "CarrierWave.clean_cached_files!"
