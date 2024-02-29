2ca07815d8079307082dbafc5d2f33a8b81efdc0
Eric Larson <ewlarson@gmail.com>
2024-02-29 12:08:07 -0600

    Map: style the addBoundsOverlay on complex geoms
    Fixes #505

--- app/assets/javascripts/geoportal/viewers/map.js
+++ app/assets/javascripts/geoportal/viewers/map.js
@@ -47,7 +47,15 @@ GeoBlacklight.Viewer.Map = GeoBlacklight.Viewer.extend({
         bounds.getSouthEast(),
         bounds.getNorthEast(),
         bounds.getNorthWest()
-      ]));
+      ],
+        {
+          color: '#3388ff',
+          dashArray: "5 5",
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0
+        }
+      ));
     }
   },
 
