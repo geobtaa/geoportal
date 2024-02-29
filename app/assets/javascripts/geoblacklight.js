39e34d0891aaea7b7accf64eb62ae13090b28ace
Eric Larson <ewlarson@gmail.com>
2024-02-29 16:20:34 -0600

    Map: update ESRI attribution

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -1,6 +1,6 @@
 // additional leaflet base layers
 GeoBlacklight.Basemaps.esri =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution: 'Esri, USGS | Esri, TomTom, FAO, NOAA, USGS',
   maxZoom: 18,
   worldCopyJump: true,
   detectRetina: true,
