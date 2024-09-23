f50a035eb6687ab3553a2655a7a1a308511b9963
Eric Larson <ewlarson@gmail.com>
2024-09-23 10:10:11 -0500

    Production Config: set default_url_options host via ENV var
    Fixes #608

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: %{"B1G Geoportal" <swadm@#{`hostname`.strip}>}}
-  config.action_mailer.default_url_options = { :host => 'geo.btaa.org' }
+  config.action_mailer.default_url_options = { :host => ENV["GEOPORTAL_DEFAULT_URL_HOST"] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
