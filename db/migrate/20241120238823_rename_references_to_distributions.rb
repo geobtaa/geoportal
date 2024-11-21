ef67cb11d9ddd7d56209497059db89c9482878ac
Eric Larson <ewlarson@gmail.com>
2024-11-20 18:11:35 -0600

    Update 20241120238823_rename_references_to_distributions.rb

--- db/migrate/20241120238823_rename_references_to_distributions.rb
+++ db/migrate/20241120238823_rename_references_to_distributions.rb
@@ -1,4 +1,4 @@
-class RenameReferencesToDistributions < ActiveRecord::Migration[7.2]
+class RenameReferencesToDistributions < ActiveRecord::Migration[7.0]
   def change
     rename_table :document_references, :document_distributions
   end
