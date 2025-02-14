ecf5d750727f3e72206dc1ee64f7732c5c5f2118
Eric Larson <ewlarson@gmail.com>
2025-02-14 13:13:06 -0600

    Item View: unify pagination links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,9 +29,11 @@ module ApplicationHelper
   def citation_date(date_string)
     date_value = ""
 
-    if date_string.size == 4
+    if date_string.blank?
+      date_value = 'n.d.'
+    elsif date_string&.size == 4
       date_value = date_string
-    elsif date_string.match?(/[-|:|\/]/)
+    elsif date_string&.match?(/[-|:|\/]/)
       begin
         date_value = Chronic.parse(date_string).to_date.strftime('%Y')
       rescue
@@ -196,5 +198,38 @@ module ApplicationHelper
     else
       '1400s-earlier'
     end
+  end
+
+  # Create a link back to the index screen, keeping the user's facet, query and paging choices intact by using session.
+  # @example
+  #   link_back_to_catalog(label: 'Back to Search')
+  #   link_back_to_catalog(label: 'Back to Search', route_set: my_engine)
+  def btaa_link_back_to_catalog(opts = { label: nil })
+    scope = opts.delete(:route_set) || self
+    query_params = search_state.reset(current_search_session.try(:query_params)).to_hash
+
+    if search_session['counter']
+      per_page = (search_session['per_page'] || blacklight_config.default_per_page).to_i
+      counter = search_session['counter'].to_i
+
+      query_params[:per_page] = per_page unless search_session['per_page'].to_i == blacklight_config.default_per_page
+      query_params[:page] = ((counter - 1) / per_page) + 1
+    end
+
+    link_url = if query_params.empty?
+                 search_action_path(only_path: true)
+               else
+                 scope.url_for(query_params)
+               end
+    label = opts.delete(:label)
+
+    if link_url =~ /bookmarks/
+      label ||= t('blacklight.back_to_bookmarks')
+    end
+
+    label ||= t('blacklight.back_to_search')
+
+    # Customized to just return the link_url
+    link_url
   end
 end
