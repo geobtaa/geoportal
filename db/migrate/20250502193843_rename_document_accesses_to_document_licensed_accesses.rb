a6082c5fb1306bcbe55290efc5fd6bf7b6c574f5
Eric Larson <ewlarson@gmail.com>
2025-06-09 11:10:48 -0500

    Update 20250502193843_rename_document_accesses_to_document_licensed_accesses.rb
    This migration should only run if the old document_accesses table is present

--- db/migrate/20250502193843_rename_document_accesses_to_document_licensed_accesses.rb
+++ db/migrate/20250502193843_rename_document_accesses_to_document_licensed_accesses.rb
@@ -1,5 +1,7 @@
 class RenameDocumentAccessesToDocumentLicensedAccesses < ActiveRecord::Migration[7.0]
   def change
-    rename_table :document_accesses, :document_licensed_accesses
+    if table_exists?(:document_accesses)
+      rename_table :document_accesses, :document_licensed_accesses
+    end
   end
 end
