a424b8c6494bad0d9397c841ae10b9ea72c163fc
Eric Larson <ewlarson@gmail.com>
2025-08-21 14:23:12 -0500

    Update geojson.js
    * Adds popover to display feature properties

--- app/assets/javascripts/geoportal/viewers/geojson.js
+++ app/assets/javascripts/geoportal/viewers/geojson.js
@@ -18,8 +18,57 @@ GeoBlacklight.Viewer.Geojson = GeoBlacklight.Viewer.Map.extend({
     var geoJSONLayer;
     var options = this.data.leafletOptions;
     $.getJSON(this.data.url, function(data) {
-      geoJSONLayer = L.geoJson(data).addTo(_this.map);
+      geoJSONLayer = L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+          // Add popup on click if properties exist
+          if (feature.properties && Object.keys(feature.properties).length > 0) {
+            var popupContent = _this.createPopupContent(feature.properties);
+            layer.bindPopup(popupContent);
+            
+            // Add hover effect
+            layer.on('mouseover', function() {
+              layer.setStyle({
+                weight: 3,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 0.7
+              });
+              if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+              }
+            });
+            
+            layer.on('mouseout', function() {
+              geoJSONLayer.resetStyle(layer);
+            });
+          }
+        }
+      }).addTo(_this.map);
       _this.map.fitBounds(geoJSONLayer.getBounds());
     });
+  },
+  
+  createPopupContent: function(properties) {
+    var content = '<div class="geojson-popup"><table class="table table-sm table-striped table-bordered">';
+    for (var key in properties) {
+      if (properties.hasOwnProperty(key)) {
+        var value = properties[key];
+        // Format the value - handle arrays, objects, and long strings
+        if (Array.isArray(value)) {
+          value = value.join(', ');
+        } else if (typeof value === 'object' && value !== null) {
+          value = JSON.stringify(value);
+        } else if (typeof value === 'string' && value.length > 100) {
+          value = value.substring(0, 100) + '...';
+        }
+        
+        content += '<tr>';
+        content += '<th class="text-nowrap">' + key + '</th>';
+        content += '<td>' + value + '</td>';
+        content += '</tr>';
+      }
+    }
+    content += '</table></div>';
+    return content;
   }
 });
