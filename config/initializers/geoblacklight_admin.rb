b44822c1a839b4a171390977e2472451f6a8bb8b
Eric Larson <ewlarson@gmail.com>
2026-01-22 11:53:52 -0600

    Remove direct upload route and related references from the admin configuration to streamline upload handling. Update Uppy integration to exclusively use presigned URLs for S3 uploads, enhancing clarity and efficiency in the upload process.

--- config/initializers/geoblacklight_admin.rb
+++ config/initializers/geoblacklight_admin.rb
@@ -29,7 +29,6 @@ module Admin
     def self.kithe_upload_data_config(toggle_value: "kithe-upload")
       {
         toggle: toggle_value,
-        upload_endpoint: Rails.application.routes.url_helpers.admin_direct_app_upload_path,
         presign_endpoint: Rails.application.routes.url_helpers.admin_presign_app_upload_path
       }
     end
